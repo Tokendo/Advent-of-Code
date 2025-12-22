7a348bed63031d5867bf206f1effab486dc72854
Tokendo <74203473+Tokendo@users.noreply.github.com>
2025-12-22 13:32:48 +0100

    run test file as expected

--- 2025/Day6/Day6.cpp
+++ 2025/Day6/Day6.cpp
@@ -5,6 +5,28 @@
 #include <chrono>
 
 
+
+long long calculate(long long arrInValues[100],int nbQty, char calcOperator)
+{
+	long long result = arrInValues[0];
+	if (calcOperator == '*')
+	{
+		for (size_t i = 1; i < nbQty; i++)
+		{
+			result *= arrInValues[i];
+		}
+	}
+	else if (calcOperator == '+')
+	{
+		for (size_t i = 1; i < nbQty; i++)
+		{
+			result += arrInValues[i];
+		}
+	}
+	
+	return result;
+}
+
 int main()
 {
 	auto timeFirstLine = std::chrono::high_resolution_clock::now();
@@ -14,17 +36,12 @@ int main()
 	std::string nb = "";
 	static long long arrValues[4][1000] = { 0 };
 	static char arrOperators[1000] = { 0 };
+	static long long arrResults[1000] = { 0 };
 	int valuesCollumns = 0, valuesDepth = 0;
-
-	/*for (size_t i = 0; i < (sizeof(arrValues) / sizeof(*arrValues)); i++)
-	{
-		arrValues[i][0] = LLONG_MAX;
-		arrValues[i][1] = LLONG_MAX;
-	}*/
 
 	printf("Reading file line by line:\n");
 	auto start = std::chrono::high_resolution_clock::now();
-	int charvalue = 0, lineIndex = 0, indexChar = 0;
+	int charvalue = 0, lineIndex = 0, indexChar = 0, operatorQty=0, nbQty=0;
 	bool firstSpaceFound = false, firstChar=true;
 	while (std::getline(inputFile, line)) {
 		indexChar = -1;
@@ -36,6 +53,7 @@ int main()
 
 		if (charvalue > 47 && charvalue < 58)
 		{
+			nbQty++;
 			int indexArr = 0;
 			// Process numeric line
 			
@@ -64,13 +82,14 @@ int main()
 		{
 			int indexArr = 0;
 			// Process numeric line
-			for (size_t i = 0; i <= line.length(); i++)
+			for (size_t i = 0; i <= line.length()-1; i++)
 			{
 				charvalue = int(line[i]);
 				if (charvalue != 32 )
 				{
 					arrOperators[indexArr] = line[i];
 					indexArr++;
+					operatorQty++;
 				}
 			}
 
@@ -82,12 +101,22 @@ int main()
 	inputFile.close();
 	auto elapsed = std::chrono::high_resolution_clock::now() - start;
 	printf("File read in %lld microseconds.\n", std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count());
+	long long total = 0,result=0;
+	for (size_t i = 0; i < operatorQty; i++)
+	{
+		long long arrCalculationValues[4] = { arrValues[0][i], arrValues[1][i], arrValues[2][i], arrValues[3][i] };
+
+		result = calculate(arrCalculationValues,nbQty, arrOperators[i]);
+		arrResults [i] = result;
+		total += result;
+	}
+
 
 
 	auto totalelapsed = std::chrono::high_resolution_clock::now() - timeFirstLine;
 	std::string outputStr = "";
 	outputStr += "Good products: \n";
-	outputStr += "Total ID: \n";
+	outputStr += "Total :"+std::to_string(total)+"\n";
 	outputStr += "Total time: " + std::to_string(std::chrono::duration_cast<std::chrono::milliseconds>(totalelapsed).count()) + " milliseconds.\n";
 	printf(outputStr.c_str());
 	std::ofstream outputFile("output.txt");
